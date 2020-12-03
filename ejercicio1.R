ff0e96154ffa4e7218c68fede51893928e35a07b
Octavio <Octaviogarcia@MacBook-Pro-de-Octavio.local>
2020-12-02 18:51:49 -0600

    ejercicio1.R

---
+++ ejercicio1.R
@@ -0,0 +1,18 @@
+install.packages("tidyverse") # Familia de paquetes para manipulación de datos
+install.packages("nycflights13") # Paquete con bases de datos
+install.packages("plyr") # Herramientas para segmentar, aplicar y combinar datos
+# Limpieza de datos
+install.packages("stringr") # Tratamiento de strings
+install.packages("assertive") # Validaciones sobre los datos
+install.packages("lubridate") # Tratamiento de fechas
+install.packages("visdat") # Visualizar datos perdidos
+# Machine learning
+install.packages("gmodels") # Matriz de confusión
+install.packages("class") # KNN
+install.packages("C50") # Decision trees
+install.packages("fpc") # K-means
+install.packages("cluster") # K-means
+# Visualización de datos
+install.packages("treemap") # Gráfica de treemap
+install.packages("rgdal") # Mapas
+install.packages("leaflet") # Mapas
